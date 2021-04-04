d5758844bce834d56c5c72a11f36a021ebbb4dfe
shahin alam <shahin786alam@yahoo.com>
2021-04-04 03:22:12 -0400

    first commit

---
+++ src/javabasics/DataTypeTest.java
@@ -0,0 +1,51 @@
+package javabasics;
+
+public class DataTypeTest {
+int k=12;
+	public static void main(String[] args) {
+		int n=13;
+		/*boolean b=true;
+		boolean bl=false;
+		int i=10;
+		short s=13;
+		byte bt=12;
+		long l=100;
+		char c='a';
+		float f=50.11f;
+		double d=12.12;
+		String s1="robiul";*/
+		
+//		int i=1;
+//		while (i<=10) {
+//		System.out.println(i);
+//		i=i+1;
+//		}
+		
+//		for(int j=1; j<=10; j++) {
+//			System.out.println(j);
+//		}
+		
+		/*int i=1;
+		do {System.out.println(i);
+		i=i+1;
+		}
+		while (i<=10); */
+		
+		int a=12;
+		int b=15;
+		String x="hello";
+		String y="world"; 
+		
+		System.out.println(a+b);
+		System.out.println(x+y);
+		System.out.println(x+y+a+b);
+		System.out.println(x+y+(a+b));
+
+	}
+	public void Test() {
+		int m=17;
+		
+	}
+	int k1=16;
+
+}
